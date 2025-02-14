f42a35f19ac9a3c11ff962ac1ef0ee24a681d46f
Daria Anisimova <anisimova32@icloud.com>
2025-02-14 16:32:57 +0300

    first commit

---
+++ my.js
@@ -0,0 +1,60 @@
+let body = document.querySelector("body");
+
+let inputDate = document.querySelector("#inputDate");
+let inputTime = document.querySelector("#inputTime");
+let submitDate = document.querySelector("#submitDate");
+let changeButton = document.querySelector("#changeButton");
+
+let textDay = document.querySelector("#textDay");
+
+function startCount(inputDateValue) {
+  let interval = setInterval(function () {
+    let currentTime = Date.now();
+    let restMills = inputDateValue.getTime() - currentTime;
+    if (restMills <= 0) {
+      clearInterval(interval);
+      textDay.textContent = "Ура, отпуск";
+      return;
+    }
+
+    let restDays = Math.floor(restMills / (1000 * 60 * 60 * 24));
+    let restHours = Math.floor((restMills / (1000 * 60 * 60)) % 24);
+    let restMinutes = Math.floor((restMills / (1000 * 60)) % 60);
+    let restSeconds = Math.floor((restMills / 1000) % 60);
+    textDay.textContent = `${restDays} дн. : ${restHours} ч. : ${restMinutes} мин. : ${restSeconds} сек.`;
+  }, 1000);
+}
+
+if (localStorage.getItem("key")) {
+  let key = JSON.parse(localStorage.getItem("key"));
+  let inputDateValue = new Date(key);
+
+  if (inputDateValue > Date.now()) {
+    startCount(inputDateValue);
+  }
+}
+
+submitDate.addEventListener("click", function () {
+  location.reload();
+  let inputDateValue = new Date(inputDate.value);
+
+  if (inputDateValue > Date.now()) {
+    localStorage.setItem("key", JSON.stringify(inputDateValue));
+    startCount(inputDateValue);
+  } else {
+    textDay.textContent = "Введите дату в будущем";
+  }
+});
+
+changeButton.addEventListener("click", function () {
+  location.reload();
+  localStorage.removeItem("key");
+  let newDate = new Date(inputDate.value);
+  localStorage.setItem("key", JSON.stringify(newDate));
+  if (newDate > Date.now()) {
+    localStorage.setItem("key", JSON.stringify(newDate));
+    startCount(newDate);
+  } else {
+    textDay.textContent = "Введите дату в будущем";
+  }
+});
