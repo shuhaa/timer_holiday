60915a5ead0e0c8a6259e4189a4a60b584ec812e
Daria Anisimova <anisimova32@icloud.com>
2025-02-15 20:17:05 +0300

    first

--- my.js
+++ my.js
@@ -1,23 +1,45 @@
 let body = document.querySelector("body");
 
 let inputDate = document.querySelector("#inputDate");
-let inputTime = document.querySelector("#inputTime");
-let submitDate = document.querySelector("#submitDate");
+
 let changeButton = document.querySelector("#changeButton");
 
 let textDay = document.querySelector("#textDay");
-let divImg = document.querySelector("#divImg");
+let textHours = document.querySelector("#textHours");
+let textMinute = document.querySelector("#textMinute");
+let textSecond = document.querySelector("#textSecond");
+let pDay = document.querySelector("#pDay");
+let pHours = document.querySelector("#pHours");
+let pMinute = document.querySelector("#pMinute");
+let pSecond = document.querySelector("#pSecond");
+let h3 = document.querySelector("#h3");
 
 function startCount(inputDateValue) {
   let interval = setInterval(function () {
     let currentTime = Date.now();
     let restMills = inputDateValue.getTime() - currentTime;
+
     if (restMills <= 0) {
       clearInterval(interval);
-      textDay.textContent = "Ура, отпуск";
-      let img = document.createElement("img");
-      img.src = "./images/animal.jpg";
-      divImg.appendChild(img);
+
+      $(document).ready(function () {
+        if ($.fn.fireworks) {
+          $(".sec").fireworks({
+            sound: true,
+            width: "100%",
+            height: "100%",
+            opacity: 0.6,
+          });
+
+          setTimeout(function () {
+            $(".sec").fireworks("destroy");
+            location.reload();
+          }, 10000);
+        } else {
+          console.log("Fireworks plugin not loaded correctly");
+        }
+      });
+
       return;
     }
 
@@ -25,7 +47,65 @@ function startCount(inputDateValue) {
     let restHours = Math.floor((restMills / (1000 * 60 * 60)) % 24);
     let restMinutes = Math.floor((restMills / (1000 * 60)) % 60);
     let restSeconds = Math.floor((restMills / 1000) % 60);
-    textDay.textContent = `${restDays} дн. : ${restHours} ч. : ${restMinutes} мин. : ${restSeconds} сек.`;
+    //делаем время строками
+    let restDaysLenght = String(restDays);
+    let restHoursLenght = String(restHours);
+    let restMinutesLenght = String(restMinutes);
+    let restSecondsLenght = String(restSeconds);
+    // текст к дням
+    if (restDaysLenght[restDaysLenght.length - 1] == "1") {
+      pDay.textContent = ` день`;
+    } else if (
+      restDaysLenght[restDaysLenght.length - 1] == "2" ||
+      restDaysLenght[restDaysLenght.length - 1] == "3" ||
+      restDaysLenght[restDaysLenght.length - 1] == "4"
+    ) {
+      pDay.textContent = `дня`;
+    } else {
+      pDay.textContent = `дней`;
+    }
+    // текст к часам
+    if (restHoursLenght[restHoursLenght.length - 1] == "1") {
+      pHours.textContent = `час`;
+    } else if (
+      restHoursLenght[restHoursLenght.length - 1] == "2" ||
+      restHoursLenght[restHoursLenght.length - 1] == "3" ||
+      restHoursLenght[restHoursLenght.length - 1] == "4"
+    ) {
+      pHours.textContent = `часа`;
+    } else {
+      pHours.textContent = `часов`;
+    }
+    // текст к минутам
+    if (restMinutesLenght[restMinutesLenght.length - 1] == "1") {
+      pMinute.textContent = `минута`;
+    } else if (
+      restMinutesLenght[restMinutesLenght.length - 1] == "2" ||
+      restMinutesLenght[restMinutesLenght.length - 1] == "3" ||
+      restMinutesLenght[restMinutesLenght.length - 1] == "4"
+    ) {
+      pMinute.textContent = `минуты`;
+    } else {
+      pMinute.textContent = `минут`;
+    }
+    // текст к секундам
+    if (restSecondsLenght[restSecondsLenght.length - 1] == "1") {
+      pSecond.textContent = `секунда`;
+    } else if (
+      restSecondsLenght[restSecondsLenght.length - 1] == "2" ||
+      restSecondsLenght[restSecondsLenght.length - 1] == "3" ||
+      restSecondsLenght[restSecondsLenght.length - 1] == "4"
+    ) {
+      pSecond.textContent = `секунды`;
+    } else {
+      pSecond.textContent = `секунд`;
+    }
+    // выводим время
+
+    textDay.textContent = `${restDays} `;
+    textHours.textContent = `${restHours}`;
+    textMinute.textContent = `${restMinutes}`;
+    textSecond.textContent = `${restSeconds} `;
   }, 1000);
 }
 
@@ -38,27 +118,40 @@ if (localStorage.getItem("key")) {
   }
 }
 
+let submitDate = document.querySelector("#submitDate");
 submitDate.addEventListener("click", function () {
-  location.reload();
   let inputDateValue = new Date(inputDate.value);
-
   if (inputDateValue > Date.now()) {
     localStorage.setItem("key", JSON.stringify(inputDateValue));
     startCount(inputDateValue);
+    location.reload();
   } else {
-    textDay.textContent = "Введите дату в будущем";
+    h3.textContent = "Введите дату в будущем";
   }
 });
 
 changeButton.addEventListener("click", function () {
-  location.reload();
   localStorage.removeItem("key");
   let newDate = new Date(inputDate.value);
   localStorage.setItem("key", JSON.stringify(newDate));
   if (newDate > Date.now()) {
-    localStorage.setItem("key", JSON.stringify(newDate));
     startCount(newDate);
+    location.reload();
   } else {
-    textDay.textContent = "Введите дату в будущем";
+    h3.textContent = "Введите дату в будущем";
   }
+});
+
+$(document).ready(function () {
+  $(".slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    centerMode: true,
+    centerPadding: "50px",
+    autoplay: true,
+    autoplaySpeed: 2000,
+    focusOnSelect: true,
+  });
+  $(".slick-arrow").text("");
 });
